1acb606b66a89459fe5e5d4cdc3f0ee0f5e376c1
Timur <minteemer@gmail.com>
2018-11-08 19:54:23 +0300

    Numerical presentation of account creation date

--- data_preparation.py
+++ data_preparation.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from datetime import datetime
+from sklearn import preprocessing
+
+TEST_SIZE = 0.3
 
 # Make Y and X train and test sets
 train = pd.read_csv('train.csv')
@@ -9,8 +13,22 @@ X = train.drop('OpenStatus', axis=1)
 
 # TODO: analyze dates
 # But for now simply drop them
-X = X.drop('PostClosedDate', axis=1).drop('PostCreationDate', axis=1) \
-    .drop('PostId', axis=1).drop('OwnerCreationDate', axis=1)
+X = X.drop('PostClosedDate', axis=1) \
+    .drop('PostCreationDate', axis=1) \
+    .drop('OwnerUserId', axis=1) \
+    .drop('PostId', axis=1)
+
+
+# prepare account creation dates
+def date_string_to_timestamp(date):
+    try:
+        return datetime.strptime(date, "%m/%d/%Y %H:%M:%S").timestamp()
+    except ValueError:  # some dates are in different format
+        return datetime.strptime(date, "%Y-%m-%d").timestamp()
+
+
+creation_timestamps = X["OwnerCreationDate"].map(date_string_to_timestamp)
+X["OwnerCreationDate"] = (creation_timestamps - creation_timestamps.mean()) / creation_timestamps.std()
 
 # TODO: analyze titles and bodyMarkdowns considering tags and not only
 # But for now simply drop them
@@ -30,5 +48,10 @@ X = X.drop('Tag1', axis=1).drop('Tag2', axis=1).drop('Tag3', axis=1) \
     .drop('Tag4', axis=1).drop('Tag5', axis=1)
 
 
+def get_normalised_data():
+    X_scaled = preprocessing.MinMaxScaler().fit_transform(X.values)
+    return train_test_split(pd.DataFrame(X_scaled), Y, test_size=TEST_SIZE)
+
+
 def get_data():
-    return train_test_split(X, Y, test_size=0.3)
+    return train_test_split(X, Y, test_size=TEST_SIZE)
--- knn.py
+++ knn.py
@@ -2,7 +2,7 @@ import data_preparation
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-X_train, X_test, y_train, y_test = data_preparation.get_data()
+X_train, X_test, y_train, y_test = data_preparation.get_normalised_data()
 
 m = KNeighborsClassifier(n_neighbors=100, p=10)
 m.fit(X_train, y_train)
