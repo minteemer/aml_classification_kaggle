66278f3839f8b8916ea8d2b9052138870ed8bbff
Timur <minteemer@gmail.com>
2018-11-09 13:10:26 +0300

    Initial solution

--- data_preparation.py
+++ data_preparation.py
@@ -5,53 +5,52 @@ from sklearn import preprocessing
 
 TEST_SIZE = 0.3
 
-# Make Y and X train and test sets
-train = pd.read_csv('train.csv')
-Y = train['OpenStatus']
-# X_test = pd.read_csv('test.csv')
-X = train.drop('OpenStatus', axis=1)
 
-# TODO: analyze dates
-# But for now simply drop them
-X = X.drop(['PostClosedDate', 'PostCreationDate', 'OwnerUserId', 'PostId'], axis=1)
+def prepare(X):
+    X = X.drop(['PostCreationDate', 'OwnerUserId'], axis=1)
+    if 'PostClosedDate' in X.columns:
+        X = X.drop('PostClosedDate', axis=1)
 
+    # prepare account creation dates
+    def date_string_to_timestamp(date):
+        try:
+            return datetime.strptime(date, "%m/%d/%Y %H:%M:%S").timestamp()
+        except ValueError:  # some dates are in different format
+            return datetime.strptime(date, "%Y-%m-%d").timestamp()
 
-# prepare account creation dates
-def date_string_to_timestamp(date):
-    try:
-        return datetime.strptime(date, "%m/%d/%Y %H:%M:%S").timestamp()
-    except ValueError:  # some dates are in different format
-        return datetime.strptime(date, "%Y-%m-%d").timestamp()
+    creation_timestamps = X["OwnerCreationDate"].map(date_string_to_timestamp)
+    X["OwnerCreationDate"] = (creation_timestamps - creation_timestamps.mean()) / creation_timestamps.std()
 
+    # Creating new column counting tags (more tags -> better post)
+    for tag_index in ['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5']:
+        X[tag_index] = X[tag_index].map(lambda x: "" if isinstance(x, float) else x)
 
-creation_timestamps = X["OwnerCreationDate"].map(date_string_to_timestamp)
-X["OwnerCreationDate"] = (creation_timestamps - creation_timestamps.mean()) / creation_timestamps.std()
+    tags = (X['Tag1'] + " " + X['Tag2'] + " " + X['Tag3'] + " " + X['Tag4'] + " " + X['Tag5']).str.strip()
+    X['Tags'] = pd.Series(tags)
 
-# TODO: analyze titles and bodyMarkdowns considering tags and not only
-# But for now simply drop them
+    X['CountTags'] = tags.map(lambda tags_str: len(tags_str.split(" ")))
+    X = X.drop(['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5'], axis=1)
 
-text_X = X[['Title', 'BodyMarkdown']]
-X = X.drop(['Title', 'BodyMarkdown'], axis=1)
-
-# Creating new column counting tags (more tags -> better post)
-for tag_index in ['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5']:
-    X[tag_index] = X[tag_index].map(lambda x: "" if isinstance(x, float) else x)
-
-tags = (X['Tag1'] + " " + X['Tag2'] + " " + X['Tag3'] + " " + X['Tag4'] + " " + X['Tag5']).str.strip()
-text_X['Tags'] = pd.Series(tags)
-
-X['CountTags'] = tags.map(lambda tags_str: len(tags_str.split(" ")))
-X = X.drop(['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5'], axis=1)
+    return X
 
 
-def get_normalised_numerical_data():
-    X_scaled = preprocessing.MinMaxScaler().fit_transform(X.values)
-    return train_test_split(pd.DataFrame(X_scaled), Y, test_size=TEST_SIZE)
+def get_train_data():
+    Z = pd.read_csv('train.csv', skipinitialspace=True, sep=',', encoding='latin', index_col='PostId')
+    X = prepare(Z.drop('OpenStatus', axis=1))
+    Y = Z['OpenStatus']
+    return X, Y
 
 
-def get_numerical_data():
-    return train_test_split(X, Y, test_size=TEST_SIZE)
+def get_test_data():
+    return prepare(pd.read_csv('test.csv', skipinitialspace=True, sep=',', encoding='latin', index_col='PostId'))
 
-
-def get_text_data():
-    return train_test_split(text_X, Y, test_size=TEST_SIZE)
+# def get_normalised_numerical_data():
+#     X_scaled = preprocessing.MinMaxScaler().fit_transform(X.values)
+#     return train_test_split(pd.DataFrame(X_scaled), Y, test_size=TEST_SIZE)
+#
+# def get_numerical_data():
+#     return train_test_split(X, Y, test_size=TEST_SIZE)
+#
+#
+# def get_text_data():
+#     return train_test_split(text_X, Y, test_size=TEST_SIZE)
--- text.py
+++ text.py
@@ -1,23 +1,56 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.preprocessing import FunctionTransformer, MultiLabelBinarizer, OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
 
 import data_preparation
 
-criterion = 'gini'
-max_depth = 8
-n_estimators = 17
+if __name__ == '__main__':
 
-text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf',
-                      GradientBoostingClassifier(n_estimators=25, learning_rate=0.005, max_depth=7)), ])
+    X_train, y_train = data_preparation.get_train_data()
 
-X_train, X_test, y_train, y_test = data_preparation.get_text_data()
-X_train = X_train["BodyMarkdown"]
-X_test = X_test["BodyMarkdown"]
-text_clf.fit(X_train, y_train)
-Y_hat = text_clf.predict(X_test)
-accuracy = accuracy_score(Y_hat, y_test)
-print('Accuracy for SGD - %.3f' % accuracy)
+    clf = make_pipeline(
+        make_column_transformer(
+            ('Title', make_pipeline(CountVectorizer(), TfidfTransformer())),
+            ('BodyMarkdown', make_pipeline(CountVectorizer(), TfidfTransformer())),
+            ('Tags', make_pipeline(CountVectorizer(), TfidfTransformer())),
+            (['ReputationAtPostCreation', 'OwnerUndeletedAnswerCountAtPostTime', 'OwnerCreationDate'], 'passthrough'),
+    #         (['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5'], make_pipeline(
+    #             DropNaTransformer(),
+    #             OneHotEncoder(),
+    #         )),
+        ),
+        LogisticRegression(),
+    )
+
+    # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.3)
+    clf.fit(X_train, y_train)
+
+    X_test = data_preparation.get_test_data()
+    Y_hat = clf.predict(X_test)
+    S = pd.DataFrame(Y_hat, columns=['OpenStatus'], index=X_test.index)
+    print(S.head())
+    S.to_csv('solution.csv')  # accuracy>0.49 on test
+
+# criterion = 'gini'
+# max_depth = 8
+# n_estimators = 17
+#
+# text_clf = Pipeline([('vect', CountVectorizer()),
+#                      ('tfidf', TfidfTransformer()),
+#                      ('clf',
+#                       GradientBoostingClassifier(n_estimators=25, learning_rate=0.005, max_depth=7)), ])
+#
+# text_clf.fit(X_train, y_train)
+# Y_hat = text_clf.predict(X_test)
+# accuracy = accuracy_score(Y_hat, y_test)
+# print('Accuracy for SGD - %.3f' % accuracy)
